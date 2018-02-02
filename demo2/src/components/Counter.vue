ca86f09c86aa217e5d523e6665fff5a2d95a164f
zxdeng <deng.zx@belle.com.cn>
2018-02-02 11:04:14 +0800

    up

---
+++ demo2/src/components/Counter.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		Count is {{ count}}
+		<button @click="increment">+</button>
+		<button @click="decrement">-</button>
+	</div>
+</template>
+<script>
+	import {mapGetters,mapActions} from 'vuex'
+	export default {
+		computed:mapGetters([
+            'count'
+		]),
+		
+		methods:mapActions([
+			'increment',
+			'decrement'
+		])
+	}
+</script>
--- demo2/src/main.js
+++ demo2/src/main.js
@@ -3,13 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from './store'
+import Counter from './components/Counter'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+  store,
+  render:h => h(Counter)
 })
